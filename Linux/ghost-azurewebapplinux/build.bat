e191458d6bd2f38e471df41b81dbffa6920e90c1
Carlos Milán Figueredo <cmilanf@hispamsx.org>
2019-02-17 19:19:07 +0100

    Folder splitted into a new repository

--- Linux/ghost-azurewebapplinux/build.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-REM Docker build for syncing version with official Ghost image
-REM %1 - Official Ghost Docker image tag
-REM %2 - (Optional) Additional build options (ex. --no-cache)
-MD %TEMP%\docker-build
-COPY /Y *.* %TEMP%\docker-build
-docker build %2 --build-arg VERSION=%1 -t cmilanf/ghost-azurewebapplinux:%1 %TEMP%\docker-build
-RD /S /Q %TEMP%\docker-build
